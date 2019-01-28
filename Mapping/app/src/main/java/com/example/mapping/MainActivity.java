d9ffada46979f498e560afa66de79b379a005728
Aaron Jenkins <JENKA17@solent.ac.uk>
2019-01-28 14:55:17 +0000

    end of day

--- Mapping/app/src/main/java/com/example/mapping/MainActivity.java
+++ Mapping/app/src/main/java/com/example/mapping/MainActivity.java
@@ -7,6 +7,10 @@ import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
@@ -23,9 +27,15 @@ public class MainActivity extends AppCompatActivity
         // This line sets the user agent, a requirement to download OSM maps
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
         setContentView(R.layout.activity_main);
+        TextView tv1 = (TextView) findViewById(R.id.tv1);
+        EditText et1 = (EditText) findViewById(R.id.et1);
+        TextView tv2 = (TextView) findViewById(R.id.tv2);
+        EditText et2 = (EditText) findViewById(R.id.et2);
+        //Button b = (Button)findViewById(R.id.btn1);
         mv = (MapView)findViewById(R.id.map1);
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(16);
         mv.getController().setCenter(new GeoPoint(51.2867,-1.07));
+
     }
 }
