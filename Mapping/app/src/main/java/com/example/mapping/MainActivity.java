1e8218865d956e78121a2de4d679bd725dd1e027
Aaron Jenkins <JENKA17@solent.ac.uk>
2019-01-29 15:08:53 +0000

    working map

--- Mapping/app/src/main/java/com/example/mapping/MainActivity.java
+++ Mapping/app/src/main/java/com/example/mapping/MainActivity.java
@@ -1,22 +1,18 @@
 package com.example.mapping;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import org.osmdroid.config.Configuration;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
     MapView mv;
     /** Called when the activity is first created. */
@@ -31,11 +27,68 @@ public class MainActivity extends AppCompatActivity
         EditText et1 = (EditText) findViewById(R.id.et1);
         TextView tv2 = (TextView) findViewById(R.id.tv2);
         EditText et2 = (EditText) findViewById(R.id.et2);
-        //Button b = (Button)findViewById(R.id.btn1);
+        Button b = (Button)findViewById(R.id.btn1);
+        b.setOnClickListener(this);
         mv = (MapView)findViewById(R.id.map1);
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(16);
         mv.getController().setCenter(new GeoPoint(51.2867,-1.07));
 
+    }
+
+    public Double parseLat (EditText geoEditText) {
+        String input = geoEditText.getText().toString();
+        try {
+            //parse string to double
+            Double latitude = Double.parseDouble(input);
+            //validate data
+            if (latitude > 90 || latitude < -90) {
+                geoEditText.setText(""); //reset text if data invalid
+                geoEditText.setHint("invalid latitude: " + input);
+                String message = "invalid latitude";
+                new AlertDialog.Builder(this).setPositiveButton("OK", null).setMessage(message).show();
+                return null;
+            }
+            return latitude;
+        } catch (Exception e) {
+            geoEditText.setText("");
+            geoEditText.setHint("invalid latitude: " + input);
+            String message = "invalid latitude: " + input;
+            new AlertDialog.Builder(this).setPositiveButton("OK", null).setMessage(message).show();
+            return null;
+        }
+    }
+
+    public Double parseLong (EditText geoEditText) {
+        String input = geoEditText.getText().toString();
+        try {
+            //parse string to double
+            Double longitude = Double.parseDouble(input);
+            //validate data
+            if (longitude > 90 || longitude < -90) {
+                geoEditText.setText(""); //reset text if data invalid
+                geoEditText.setHint("invalid longitude: " + input);
+                String message = "invalid longitude";
+                new AlertDialog.Builder(this).setPositiveButton("OK", null).setMessage(message).show();
+                return null;
+            }
+            return longitude;
+        } catch (Exception e) {
+            geoEditText.setText("");
+            geoEditText.setHint("invalid longitude: " + input);
+            String message = "invalid longitude: " + input;
+            new AlertDialog.Builder(this).setPositiveButton("OK", null).setMessage(message).show();
+            return null;
+        }
+    }
+
+    public void onClick(View view) {
+        EditText latEditText = (EditText) findViewById(R.id.et1);
+        EditText lonEditText = (EditText) findViewById(R.id.et2);
+
+        //get values and update location
+        Double lon = parseLat(lonEditText);
+        Double lat = parseLat(latEditText);
+        mv.getController().setCenter(new GeoPoint(lat, lon));
     }
 }
